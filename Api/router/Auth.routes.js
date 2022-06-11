b9dc3fb83e0e7147ead44a60c5d5d5ba515f0942
Saraediani <sara.pur187@gmail.com>
2022-06-11 15:11:03 +0100

    api router

---
+++ Api/router/Auth.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+const userRouter = express.Router();
+
+// import authController from "../controllers/authController.js";
+
+// router.post("/register", authController.register);
+// router.post("/login", authController.login);
+// router.route("/logout").get(authController.logout);
+
+export default userRouter;
